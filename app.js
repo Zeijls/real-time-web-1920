db940d7f5f2c8303d892eaa65227c45e53fef1bf
Simone van Zeijl <simonev.zeijl@hotmail.nl>
2020-05-01 13:11:29 +0200

    .

--- app.js
+++ app.js
@@ -41,19 +41,25 @@ const ioInstance = require("socket.io")(server);
 const path = require("path");
 const cookieParser = require("cookie-parser");
 
-const routes = require("./routes/routes");
+const routes = require("./server/routes/routes");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-// app.use(express.static(path.join(__dirname, "/static")));
 app.use(express.static(__dirname + "/static"));
 
 app.use(cookieParser());
 
 app.use(routes);
 
+// //user bijhouden
+// //https://stackoverflow.com/questions/18335028/socket-io-how-to-prompt-for-username-and-save-the-username-in-an-array
+// let connectedUsers = [];
+// let apiResults = [];
+
 ioInstance.on("connection", function (socket) {
+  //https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   console.log("socket created");
 
   let userName = "anonymous";
@@ -89,6 +95,58 @@ ioInstance.on("connection", function (socket) {
     console.log("message: " + msg);
     ioInstance.emit("chat message", `${userName}: ${msg}`);
   });
+
+  socket.on("start game", async function (id) {
+    if (connectedUsers.length === 1) {
+      apiResults = await getData();
+      console.log(apiResults.length);
+      console.log("Game Started!!!");
+      // playSong();
+    } else {
+      // socket
+    }
+  });
+
+  // Data;
+  async function getData(apiResults) {
+    const accessToken = req.cookies.ACCESS_TOKEN;
+
+    console.log("hoi");
+    const headers = {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+
+    try {
+      const spotifyResponse = await fetch(
+        "https://api.spotify.com/v1/me/tracks",
+        headers
+      );
+      let data = await spotifyResponse.json();
+      data = data.items;
+      const cleanedData = cleanItems(data);
+      res.send(cleanedData);
+      console.log(cleanedData);
+    } catch (err) {
+      console.log("error fetching songs of user: ", err);
+    }
+  }
+
+  // function playSong() {
+  //   // radom track gekozen
+  //   // naar iedere speler gestuurd
+  //   // random generator aangeroepen
+
+  //   // socket broadcast emit random song
+  // }
+
+  //   function randomSongGenerator(gameResults) {
+  //     //radom track
+  //     const randomNumber = Math.floor(Math.random() * movieListLength);
+  //   }
 });
 
 server.listen(port, () => {
---
+++ static/index.js
@@ -0,0 +1,54 @@
+const socket = io();
+socket.emit("connect", 1);
+
+// Basic Chat
+const usernameInput = document.getElementById("username");
+const usernameForm = document.getElementById("usernameForm");
+usernameForm.addEventListener("submit", setUsername);
+
+function setUsername(event) {
+  event.preventDefault();
+  // const userInputField = document.getElementById("usernameInput");
+  console.log(usernameInput.value);
+  socket.emit("set user", username.value());
+  // return false;
+  // e.preventDefault(); // prevents page reloading
+  // socket.emit("set user", usernameInput.value());
+  // return false;
+}
+
+const messageForm = document.getElementById("messageForm");
+messageForm.addEventListener("submit", inputText);
+
+function inputText(event) {
+  event.preventDefault();
+  const messageInputField = document.getElementById("message");
+  console.log(messageInputField.value);
+  socket.emit("chat message", messageInputField.value);
+}
+
+socket.on("server message", function (msg) {
+  const messages = document.getElementById("messages");
+  messages.insertAdjacentHTML(
+    "beforeend",
+    `<li class="server-message">${msg}</li>`
+  );
+});
+
+socket.on("chat message", function (msg, randomColor) {
+  const messages = document.getElementById("messages");
+  messages.insertAdjacentHTML(
+    "beforeend",
+    `<li class="chat-message" style="border: 2px solid ${randomColor};" >${msg}</li>`
+  );
+});
+
+// Start game
+// const startGame = document.getElementById("Start Game");
+
+// startGame.addEventListener("click", function () {
+//   console.log("Game started!");
+//   event.preventDefault();
+//   const username = document.getElementById("username");
+//   socket.emit("start game", username.value);
+// });
